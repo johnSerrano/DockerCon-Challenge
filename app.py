b85dedd14cb172c04fbac6d95591b149246c9a57
John Serrano <johnserrano404@gmail.com>
2016-05-31 16:39:15 -0700

    training and results webpage working

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, session
 from threading import Thread
 from network import process_network
-from flask_socketio import SocketIO, emit, send
+from flask_socketio import SocketIO, emit, send, join_room, rooms, disconnect
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode="threading")
 
 
 @app.route('/')
@@ -22,18 +22,22 @@ def runnetwork():
 	content = request.get_json()
 	if not content:
 		return "No JSON posted"
+	print "*****RUNNING NEW THREAD*****"
 	t = Thread(target=process_network, args=(content, progress_socket_callback))
 	t.start()
 	return "JSON posted successfully"
 	return process_network(content, progress_socket_callback)
 
+
 @app.route('/results')
 def results():
 	return render_template("results.html")
 
+
 @app.route('/test_post.html')
 def test_post():
 	return render_template("test_post.html")
+
 
 @app.route('/social.css')
 def css():
@@ -42,15 +46,33 @@ def css():
 
 #use websockets to post the progress and results of the network
 def progress_socket_callback(progress):
-	# send(progress)
+	socketio.emit('progress', progress)
 	if progress["done"]:
 		print "Done!",
 	print str(progress["current_epoch"]) + '/' + str(progress["total_epochs"])
 	print str(progress["val_acc"])
 
-@socketio.on('connect', namespace='/test')
+
+@socketio.on('connect')
 def test_connect():
     print('socketio connected')
+
+
+@socketio.on('join')
+def join(message):
+    join_room(message['room'])
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('room joined',
+         {'data': 'In rooms: ' + ', '.join(rooms()),
+          'count': session['receive_count']})
+
+
+@socketio.on('disconnect request')
+def disconnect_request():
+	session['receive_count'] = session.get('receive_count', 0) + 1
+	print "****disconnected****"
+	disconnect()
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
